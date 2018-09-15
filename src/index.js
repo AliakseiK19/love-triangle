6b4ad578569adcf07fa38bb4f8048a04f102c518
AliakseiK19 <positive_on@yahoo.com>
2018-09-15 15:05:40 +0300

    crazy task

--- src/index.js
+++ src/index.js
@@ -4,4 +4,24 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let result = 0;
+   preferences.unshift(0);
+   let a = 1;
+   while ((a < preferences.length) && (preferences[a + 1] == preferences[a]))
+     { 
+      a++; 
+      if (a == preferences.Length - 1) { return result; }      
+      }
+   for(let a = 1; a < preferences.length; a++){  
+         
+       let b = preferences[a];
+       let c = preferences[b];
+       if(a==preferences[c])
+       {
+         result ++;
+       }        
+   }
+   result = result/3;
+   result = result -(result%1);
+   return result;
 };
